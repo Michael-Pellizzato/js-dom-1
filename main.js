c235251d7484d5633a223dd8fc2cf10b2941f3b8
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-01-20 11:55:54 +0100

    preparazione esercizio da correggere

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 
+let element = document.getElementById('lampadina')
+let btn = document.getElementById('btnLampadina')
+
+btn.addEventListener('click', function(){
+    console.log( element )
+    element.src = "./imgs/yellow_lamp.png"
+})
